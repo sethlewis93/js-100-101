7eeb30eb51659dac811ce78d67b60d137c06eead
Seth Lewis <lewis.sethc@pm.me>
2022-09-28 07:56:21 -0400

    refactored calc asking for more input

--- lesson-2/calculator.js
+++ lesson-2/calculator.js
@@ -11,69 +11,67 @@ function invalidNumber(number) {
   return number.trimStart() === "" || Number.isNaN(Number(number));
 }
 
-// Ask and receive the first number
-prompt("Welcome to calculator!");
-prompt("Please enter the first number.");
-let num1 = readline.question();
+while (true) {
+  // Ask and receive the first number
+  prompt("Welcome to calculator!");
+  prompt("Please enter the first number.");
+  let num1 = readline.question();
 
-// Validate first number
-while (invalidNumber(num1)) {
-  prompt("Hmmm.... that doesn't look like a valid number");
-  num1 = readline.question();
-}
+  // Validate first number
+  while (invalidNumber(num1)) {
+    prompt("Hmmm.... that doesn't look like a valid number");
+    num1 = readline.question();
+  }
 
-// Ask and receive second number.
-prompt("Please enter the second number");
-let num2 = readline.question();
+  // Ask and receive second number.
+  prompt("Please enter the second number");
+  let num2 = readline.question();
 
-// Validate the second number
-while (invalidNumber(num2)) {
-  prompt("Hmm... that doesn't look like a valid number");
-  num2 = readline.question();
-}
+  // Validate the second number
+  while (invalidNumber(num2)) {
+    prompt("Hmm... that doesn't look like a valid number");
+    num2 = readline.question();
+  }
 
-// User selects the operations
-console.log(`=> What operation would you like to perform on these numbers? You can select:
+  // User selects the operations
+  console.log(`=> What operation would you like to perform on these numbers? You can select:
+  
+      "1" for Addition, 
+      "2" for Subtraction, 
+      "3" for Multiplication, or
+      "4" for Division.
+  
+  `);
 
-    "1" for Addition, 
-    "2" for Subtraction, 
-    "3" for Multiplication, or
-    "4" for Division.
+  // Validate operation input
+  let operation = readline.question();
+  while (!["1", "2", "3", "4"].includes(operation)) {
+    prompt("Please select a valid operation.");
+    operation = readline.question();
+  }
 
-`);
+  // Perform the operations
+  let output;
+  switch (operation) {
+    case "1":
+      output = `The ouput is ${Number(num1) + Number(num2)}`;
+      break;
+    case "2":
+      output = `The output is ${Number(num1) - Number(num2)}`;
+      break;
+    case "3":
+      output = `The output is ${Number(num1) * Number(num2)}`;
+      break;
+    case "4":
+      output = `The output is ${Number(num1) / Number(num2)}`;
+      break;
+  }
 
-// Validate operation input
-let operation = readline.question();
-while (!["1", "2", "3", "4"].includes(operation)) {
-  prompt("Please select a valid operation.");
-  operation = readline.question();
-}
+  // Print the output
+  console.log(output);
 
-// Perform the operations
-let output;
-switch (operation) {
-  case "1":
-    output = `The ouput is ${Number(num1) + Number(num2)}`;
-    break;
-  case "2":
-    output = `The output is ${Number(num1) - Number(num2)}`;
-    break;
-  case "3":
-    output = `The output is ${Number(num1) * Number(num2)}`;
-    break;
-  case "4":
-    output = `The output is ${Number(num1) / Number(num2)}`;
-    break;
-}
-
-// Print the output
-console.log(output);
-
-// Ask the user if they'd like to do another calculation
-prompt("Would you like to perform another calculation? Yes or no");
-let anotherCalc = readline.question();
-if (anotherCalc.toLowerCase() === "yes") {
-  anotherCalc = readline.question();
-} else {
-  process.exit(1);
+  // Ask the user if they'd like to do another calculation
+  prompt("Would you like to perform another calculation (y/n)?");
+  let anotherCalc = readline.question();
+  if (anotherCalc !== "y") break;
 }
