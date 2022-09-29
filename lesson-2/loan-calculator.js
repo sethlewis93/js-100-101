d68a43b70148dcebbcf967905d341a3c48d1663c
Seth Lewis <lewis.sethc@pm.me>
2022-09-28 21:00:37 -0400

    added validation for all 3inputs

--- lesson-2/loan-calculator.js
+++ lesson-2/loan-calculator.js
@@ -1,25 +1,41 @@
 let readline = require("readline-sync");
 
-// let m = p * (j / (1 - Math.pow((1 + j), (-n))));
-
 // Shorthand for readline.question()
 let ask = readline.question;
 
 // Get principal
-let principal = Number(ask("What is the principal amount of the loan?: ")); // <- potential guard case: user putting "$" in front of the integer
+let principal = Number(ask("What is the principal amount of the loan?: "));
+if (isNaN(principal)) {
+  console.log("Invalid input: please enter the loan amount in numerals only");
+  principal = Number(ask("What is the principal amount of the loan?: "));
+}
 console.log(`PRINCIPAL: $${principal}`);
 
 // Get APR, convert to monthly interest rate
+let monthlyInterest =
+  Number(ask("What is the current APR?: ") / 100 / 12) * 100;
 
-let monthlyInterest = Number(ask("What is the current APR?: ") / 100 / 12) * 100; // <- https://www.thebalancemoney.com/calculate-monthly-interest-315421
+if (isNaN(monthlyInterest)) {
+  console.log("Invalid input: pelase enter the APR in numerals only");
+  monthlyInterest = Number(ask("What is the current APR?: ") / 100 / 12) * 100;
+}
 console.log(`Monthly APR: ${monthlyInterest}%`);
 
-// Get duration of loan and conver to months
-let durationInMonths = Number(ask("What is the length of the loan in months?: "));
-console.log(`DURATION: ${durationInMonths} months`)
+// Get duration of loan and convert to months
+let durationInMonths = Number(
+  ask("What is the length of the loan in months?: ")
+);
+if (!Number.isInteger(durationInMonths) || isNaN(durationInMonths)) {
+  console.log(
+    "Invalid input: please enter the duration of the loan in months only without decimal places."
+  );
+  durationInMonths = Number(ask("What is the length of the loan in months?: "));
+}
+console.log(`DURATION: ${durationInMonths} months`);
 
 // Calculate the monthly payment
 let monthlyPayment =
-principal * (monthlyInterest / (1 - Math.pow((1 + monthlyInterest), (-durationInMonths)))); // <- PROBLEM HERE: principal * monthlyInterest is the only calculation running. Need to find out why.
+  principal *
+  (monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -durationInMonths))); // <- PROBLEM HERE: principal * monthlyInterest is the only calculation running. Need to find out why.
 
 console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
