2fa314d96f29b27332b481d5c4a1bbf70fa62467
Seth Lewis <lewis.sethc@pm.me>
2022-09-28 20:36:08 -0400

    refactoring on branch
    refactored calc on branch

--- lesson-2/loan-calculator.js
+++ lesson-2/loan-calculator.js
@@ -2,30 +2,24 @@ let readline = require("readline-sync");
 
 // let m = p * (j / (1 - Math.pow((1 + j), (-n))));
 
-function ask(message) {
-  console.log(`=> ${message}`);
-}
-
 // Shorthand for readline.question()
-let input = readline.question;
+let ask = readline.question;
 
 // Get principal
-ask("What is the principal amount of the loan?");
-let principal = input();
+let principal = Number(ask("What is the principal amount of the loan?: ")); // <- potential guard case: user putting "$" in front of the integer
 console.log(`PRINCIPAL: $${principal}`);
 
 // Get APR, convert to monthly interest rate
-ask("What is the current APR?");
-let monthlyInterest = (input() / 100 / 12) * 100; // <- https://www.thebalancemoney.com/calculate-monthly-interest-315421
-console.log(`APR: ${monthlyInterest}`);
+
+let monthlyInterest = Number(ask("What is the current APR?: ") / 100 / 12) * 100; // <- https://www.thebalancemoney.com/calculate-monthly-interest-315421
+console.log(`Monthly APR: ${monthlyInterest}%`);
 
 // Get duration of loan and conver to months
-ask("What is the length of the loan in years?")
-let durationInMonths = input() * 12;
+let durationInMonths = Number(ask("What is the length of the loan in months?: "));
 console.log(`DURATION: ${durationInMonths} months`)
 
 // Calculate the monthly payment
 let monthlyPayment =
-  principal * (monthlyInterest / (1 - Math.pow((1 + monthlyInterest), (-durationInMonths))));
+principal * (monthlyInterest / (1 - Math.pow((1 + monthlyInterest), (-durationInMonths)))); // <- PROBLEM HERE: principal * monthlyInterest is the only calculation running. Need to find out why.
 
 console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
