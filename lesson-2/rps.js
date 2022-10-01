27c14cda2f34603dc5ef4831571e55b930b79e63
Seth Lewis <lewis.sethc@pm.me>
2022-10-01 15:19:55 -0400

    basic rps game. need to address comments

---
+++ lesson-2/rps.js
@@ -0,0 +1,54 @@
+let readline = require("readline-sync");
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+// Array of choices
+const VALID_CHOICES = ["rock", "paper", "scissors"];
+
+// Game function
+function playGame(userInput, computerInput) {
+  if (
+    (userInput === "rock" && computerInput === "scissors") ||
+    (userInput === "paper" && computerInput === "rock") ||
+    (userInput === "scissors" && computerInput === "paper")
+  ) {
+    prompt("You win!");
+  } else if (
+    (userInput === "rock" && computerInput === "paper") ||
+    (userInput === "paper" && computerInput === "scissors") ||
+    (userInput === "scissors" && computerInput === "rock")
+  ) {
+    prompt("Computer wins!");
+  } else {
+    prompt("It's a tie");
+  }
+}
+
+// <-------------------------------------------------- *** -------------------------------------------------->
+
+while (true) {
+  // Get user input
+  prompt("Rock, paper, or scissors: take your pick");
+  let userChoice = readline.question();
+
+  // Validate user input
+  while (!VALID_CHOICES.includes(userChoice)) {
+    prompt("Invalid choice. Please choose 'rock', 'paper', or 'scissors'");
+    userChoice = readline.question();
+  }
+
+  // Get computer choice
+  const RANDOM_CHOICE_INDEX = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[RANDOM_CHOICE_INDEX];
+
+  playGame(userChoice, computerChoice);
+
+  // Offer to play again
+  prompt("Would you like to play again?");
+  let answer = readline.question();
+  if (answer === "n") break; // <-- need to make this more robust
+}
+
+// We used a while loop with an always-true condition and a break statement to decide whether to replay the game. Can you rewrite the loop so that we don't need to use the break statement to stop the loop?
