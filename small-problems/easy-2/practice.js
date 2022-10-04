755b32753cf82dfe363390750d55dd19a972b6e7
Seth Lewis <lewis.sethc@pm.me>
2022-10-04 07:39:49 -0400

    adds exercises

--- small-problems/easy-2/practice.js
+++ small-problems/easy-2/practice.js
@@ -18,12 +18,35 @@ function greetings(arr, obj) {
 
 // Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
 
-const prompt = (message) => console.log(message);
+// const prompt = (message) => console.log(message);
 
-prompt("What's your name?");
-let name = readline.question();
-name.includes("!")
-  ? prompt(
-      `HELLO ${name.replace("!", ".").toUpperCase()} WHY ARE WE SCREAMING?`
-    )
-  : prompt(`Hello ${name}`);
+// prompt("What's your name?");
+// let name = readline.question();
+// name.includes("!")
+//   ? prompt(
+//       `HELLO ${name.replace("!", ".").toUpperCase()} WHY ARE WE SCREAMING?`
+//     )
+//   : prompt(`Hello ${name}`);
+
+// <-------------------------------------------------- *** -------------------------------------------------->
+// <-------------------------------------------------- *** -------------------------------------------------->
+
+// Create a function that takes two arguments, multiplies them together, and returns the result.
+
+function multiply(multiplicand, multiplier) {
+  return multiplicand * multiplier;
+}
+
+// console.log(multiply(3, 5));
+
+// <-------------------------------------------------- *** -------------------------------------------------->
+// <-------------------------------------------------- *** -------------------------------------------------->
+
+// Using the multiply() function from the "Multiplying Two Numbers" problem, write a function that computes the square of its argument (the square is the result of multiplying a number by itself).
+
+function square(n) {
+  return multiply(n, n);
+}
+
+console.log(square(5) === 25); // logs true
+console.log(square(-8) === 64); // logs true
