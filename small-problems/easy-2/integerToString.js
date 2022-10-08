f1c923c6a340a851b433d629588602730647609b
Seth Lewis <lewis.sethc@pm.me>
2022-10-08 14:04:15 -0400

    initializes lesson-3 dir, adds practice code

---
+++ small-problems/easy-2/integerToString.js
@@ -0,0 +1,18 @@
+const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function integerToString(int) {
+  let result = "";
+
+  do {
+    // Retrieve the rightmost digit
+    let remainder = int % 10;
+    // Chop the rightmost digit
+    int = Math.floor(int / 10);
+    // Set `result` to the righmost digit and stringify it
+    result = DIGITS[remainder] + result;
+  } while (int > 0);
+
+  return result;
+}
+
+console.log(typeof integerToString(4321));
--- small-problems/easy-2/practice.js
+++ small-problems/easy-2/practice.js
@@ -280,3 +280,43 @@ function stringToSignedInteger(str) {
 function stringToSignedInteger(string) {
   return Math.floor(string);
 }
+
+// <-------------------------------------------------- *** -------------------------------------------------->
+// <-------------------------------------------------- *** -------------------------------------------------->
+
+// Write a function that converts a non-negative integer value (e.g., 0, 1, 2, 3, and so on) to the string representation of that integer.
+
+// You may not use any of the standard conversion functions available in JavaScript, such as String(), Number.prototype.toString, or an expression such as '' + number. Your function should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
+
+// INPUT = non-negative number
+// OUTPUT = string representation of the number
+
+// PROBLEM RESTATED:
+// --> Take any non-negeative integer and return the string representation of that number
+
+//
+
+// function integerToString(int) {
+//   return (int += "");
+// }
+
+// integerToString(4321); // "4321"
+// integerToString(0); // "0"
+// integerToString(5000); // "5000"
+// let whatType = integerToString(1234567890); // "1234567890"
+// console.log(typeof whatType);
+
+const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function integerToString(number) {
+  let result = "";
+
+  do {
+    let remainder = number % 10;
+    number = Math.floor(number / 10);
+
+    result = DIGITS[remainder] + result;
+  } while (number > 0);
+
+  return result;
+}
