554518d3b544fadb919c8f6c6aeacee6e761c00b
Seth <Seth@Micahs-iMac-2.home>
2022-11-05 09:55:10 -0400

    adds basic functioning program code

--- lesson-2/calculators/newLoanCalculator.js
+++ lesson-2/calculators/newLoanCalculator.js
@@ -1,13 +1,24 @@
 // Re-doing calc exercise for review.
+let readline = require("readline-sync");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-let principalAmount = 100000;
-let loanDuration = 120; // Duration in months
-let monthlyInterestRate = 0.005; // (6 / 100) / 12
+prompt("What is the principal of the loan?");
+let principalAmount = readline.question();
+
+prompt("What is the duration of the loan in months?");
+let loanDuration = readline.question();
+
+prompt("What is the interest rate (APY)?");
+let annualInterestRate = readline.question();
 /** CALC FORMULA */
+
+let monthlyInterestRate = annualInterestRate / 100 / 12;
 
 let monthlyPayment =
   principalAmount *
   (monthlyInterestRate /
     (1 - Math.pow(1 + monthlyInterestRate, -loanDuration)));
 
-console.log(monthlyPayment.toFixed(2));
+console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
