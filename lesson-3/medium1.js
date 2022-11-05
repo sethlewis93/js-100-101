7e738608db57db902b279fa56dc8daf5bc096ccd
Seth <Seth@Micahs-iMac-2.home>
2022-11-05 14:23:36 -0400

    adds code that pads a hard-coded string 1 time to the right

--- lesson-3/medium1.js
+++ lesson-3/medium1.js
@@ -30,22 +30,31 @@
 // String that is mutated and printed 10 times.
 
 // ALGORITHM:
-// Take a string and print it 10 times
+// Take a string and print it {x} times
 // Each time the string is printed:
 // --> Add 1 additional indentation to the left of the string than previous str.
 // --> Print it on a new line.
 
 // CODE:
 
-function displayString(str) {
-  let iterator = 0;
-  while (iterator < 10) {
-    console.log(str);
-    iterator += 1;
-  }
-}
+// function displayString(str) {
+//   let iterator = 0;
+//   while (iterator < 10) {
+//     console.log(str);
+//     iterator += 1;
+//   }
+// }
 
-displayString("Flintstones Rock");
+// displayString("Flintstones Rock");
+
+// Add JUST ONE indentation to ALL strings:
+// --> Get string target length (str.length + 1)
+// Set " " padding
+// Log the value
+
+let flintstones = "Flintstones Rock!";
+console.log(flintstones);
+console.log(flintstones.padStart(flintstones.length + 1, " "));
 
 // --> MY SOLUTION(S) <--
 
