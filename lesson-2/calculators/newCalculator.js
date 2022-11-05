c5fdb470f583a08faa1c9059ddb54b4e7ad722ab
Seth <Seth@Micahs-iMac-2.home>
2022-11-04 20:44:34 -0400

    removes initial comments

--- lesson-2/calculators/newCalculator.js
+++ lesson-2/calculators/newCalculator.js
@@ -1,9 +1,3 @@
-// Ask the user for the first number.
-// Ask the user for the second number.
-// Ask the user for an operation to perform.
-// Perform the operation on the two numbers.
-// Print the result to the terminal.
-
 const readline = require("readline-sync");
 
 function isInvalidNumber(num) {
