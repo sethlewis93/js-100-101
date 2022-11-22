de215f0526907b1a031906c697e0ec0ea43a4af4
Seth Lewis <lewis.sethc@pm.me>
2022-11-21 22:13:10 -0500

    adds initializeBoard function to start game with empty board

--- lesson-6/tic-tac-toe/tic-tac-toe.js
+++ lesson-6/tic-tac-toe/tic-tac-toe.js
@@ -1,34 +1,13 @@
+let readline = require("readline-sync");
+
+function prompt(message) {
+  console.log(`➡️ ${message}`);
+}
+
 /**
  ** DISPLAY AN EMPTY BOARD **
  */
 
-// Set nine "squares" in a nested object. Each prop has a an "empy" boolean k/v.
-let squares = {
-  1: { set: false },
-  2: { set: true },
-  3: { set: false },
-  4: { set: false },
-  5: { set: false },
-  6: { set: false },
-  7: { set: false },
-  8: { set: true },
-  9: { set: false },
-};
-
-// First implementation of marking "X"
-Object.values(squares).forEach((square) => {
-  /// IF the square is set,
-  // -- log "x" to the console: else, leave blank
-  //   for (let squareNum in square) {
-  //     if (square[squareNum]) {
-  //       console.log("X");
-  //     } else {
-  //       console.log(" ");
-  //     }
-  //   }
-});
-
-// Box method
 function printBoard(str) {
   let horizontalRule = "+" + "-".repeat(str.length + 2) + "";
 
@@ -44,5 +23,39 @@ function printBoard(str) {
   console.log(horizontalRule.repeat(3) + "+");
 }
 
-// printBoard("");
-printBoard("O");
+// Set nine "squares" in a nested object. Each prop has a an "empy" boolean k/v.
+let squares = {
+  1: { set: false },
+  2: { set: true },
+  3: { set: false },
+  4: { set: false },
+  5: { set: false },
+  6: { set: false },
+  7: { set: false },
+  8: { set: true },
+  9: { set: false },
+};
+
+function initializeBoard() {
+  // init and populate the printBoard function with an empty string
+  return "";
+}
+
+// Start the game:
+prompt("Let's play Tic-Tac-Toe!");
+
+let board = initializeBoard();
+printBoard(board);
+
+// First implementation of marking "X"
+Object.values(squares).forEach((square) => {
+  /// IF the square is set,
+  // -- log "x" to the console: else, leave blank
+  //   for (let squareNum in square) {
+  //     if (square[squareNum]) {
+  //       console.log("X");
+  //     } else {
+  //       console.log(" ");
+  //     }
+  //   }
+});
